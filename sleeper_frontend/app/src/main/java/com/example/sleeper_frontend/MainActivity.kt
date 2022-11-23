7acf0ed416342523703e2b225539681d37458523
HyeonJi <jhg050908@naver.com>
2022-11-24 01:25:12 +0900

    홈 배경화면 설정

--- sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/MainActivity.kt
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.sleeper_frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.example.sleeper_frontend.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val window = window
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.activityMainLayout.setBackgroundResource(R.drawable.main_background)
     }
 }
