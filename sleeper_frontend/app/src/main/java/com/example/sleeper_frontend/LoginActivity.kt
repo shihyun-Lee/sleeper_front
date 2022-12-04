1b9331068591acb66f5c5a80fa6e00d7917162ad
HyeonJi <jhg050908@naver.com>
2022-12-04 12:50:07 +0900

    로그인 xml파일과 Activity 추가

---
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.sleeper_frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import com.example.sleeper_frontend.databinding.ActivityLoginBinding
+import com.example.sleeper_frontend.databinding.ActivityMainBinding
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        super.onCreate(savedInstanceState)
+
+        val window = window
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        val binding: ActivityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.activityLoginLayout.setBackgroundResource(R.drawable.main_background)
+    }
+}
