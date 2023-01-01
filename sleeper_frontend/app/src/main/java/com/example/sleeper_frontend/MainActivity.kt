6bb01eed2226754a4d58faa4c27fa988f8b37b67
HyeonJi <jhg050908@naver.com>
2023-01-02 03:32:38 +0900

    # Fix : Popup 액티비티를 다이얼로그로 수정

--- sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/MainActivity.kt
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.sleeper_frontend
 
+import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
@@ -7,6 +9,10 @@ import android.view.WindowManager
 import com.example.sleeper_frontend.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val popup: Dialog = Dialog(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
@@ -15,9 +21,14 @@ class MainActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.activityMainLayout.setBackgroundResource(R.drawable.main_background)
+
+        binding.btnShowMore.setOnClickListener {
+            popup.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            popup.setContentView(R.layout.popup_dialog)
+        }
     }
 }
