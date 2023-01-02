175689ea80389ac56a70f2bb2d6ef6ab6863c5ee
HyeonJi <jhg050908@naver.com>
2023-01-02 14:12:47 +0900

    # Feat : MainActivity 더보기 버튼 눌렀을 때 Popup 다이얼로그 표시

--- sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/MainActivity.kt
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.sleeper_frontend
 
 import android.app.Dialog
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
@@ -11,7 +10,6 @@ import com.example.sleeper_frontend.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val popup: Dialog = Dialog(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -27,8 +25,16 @@ class MainActivity : AppCompatActivity() {
         binding.activityMainLayout.setBackgroundResource(R.drawable.main_background)
 
         binding.btnShowMore.setOnClickListener {
-            popup.requestWindowFeature(Window.FEATURE_NO_TITLE)
-            popup.setContentView(R.layout.popup_dialog)
+            clickBtnPopup()
         }
+    }
+
+    private fun clickBtnPopup() {
+        val popup = Dialog(this)
+        popup.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        popup.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        popup.setContentView(R.layout.popup_dialog)
+
+        popup.show()
     }
 }
