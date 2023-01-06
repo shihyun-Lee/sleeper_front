70f552562e9ef3ca61aa89c677568a5091e39a48
HyeonJi <jhg050908@naver.com>
2023-01-06 16:13:51 +0900

    # Feat : 다이어리 프래그먼트 코틀린 파일 작성

--- sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/DiaryFragment.kt
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/DiaryFragment.kt
@@ -5,55 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sleeper_frontend.databinding.FragmentDiaryBinding
+import com.example.sleeper_frontend.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DiaryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DiaryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentDiaryBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false)
+
+        binding = FragmentDiaryBinding.inflate(inflater, container, false)
+
+        binding.btnShowMore.setOnClickListener {
+            clickBtnPopup()
+        }
+
+        binding.btnSaveDiary.setOnClickListener {
+            saveDiary()
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DiaryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DiaryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    private fun clickBtnPopup() {
+        val popup : PopupDialogFragment = PopupDialogFragment().getInstance()
+        activity?.supportFragmentManager?.let { fragmentManager ->
+            popup.show(
+                fragmentManager,
+                "tag"
+            )
+        }
     }
+
+    private fun saveDiary() {
+        //네트워크 통신 구현
+        //해당 날짜이면 diary 수정 가능하도록.
+    }
+
 }
