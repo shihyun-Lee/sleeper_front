57541b219c9cc635ec1b9e10195cf8c0c17793fc
shihyun-Lee <jeenii22@naver.com>
2023-01-21 20:04:42 +0900

    # Feat : mypage 작동 구현

--- sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/MypageFragment.kt
+++ sleeper_frontend/app/src/main/java/com/example/sleeper_frontend/MypageFragment.kt
@@ -2,19 +2,23 @@
 package com.example.sleeper_frontend
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.sleeper_frontend.databinding.FragmentMypageBinding
 
-class MypageFragment : Fragment(R.layout.fragment_mypage) {
+class MypageFragment : androidx.fragment.app.Fragment(R.layout.fragment_mypage) {
+    private lateinit var binding: FragmentMypageBinding //변수선언
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentMypageBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        return binding.root
+
+
     }
 
 }
